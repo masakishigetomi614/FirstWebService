4ae261b30027c39c5612f53e56546291ff083d16
masaki.shigetomi <masaki.shigetomi@gmail.com>
2024-10-12 20:48:53 +0900

    起動設定定義の追加

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// サービスの登録 (必要に応じて追加)
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// ルーティングの設定
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
